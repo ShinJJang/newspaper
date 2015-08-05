8989e44cde6cb3ad03ad1e2045796a4e9f1470dc
Shin JJang <makao.rule@gmail.com>
2015-08-05 14:58:42 +0900

    Add dummy sign up page

--- news/views.py
+++ news/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def index(request):
     return render(request, "index.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
+
+
+def signup_submit(request):
+    return redirect("index")
--- newspaper/urls.py
+++ newspaper/urls.py
@@ -6,7 +6,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'newspaper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'news.views.index', name='index'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^admin/',         include(admin.site.urls)),
+    url(r'^$',              'news.views.index', name='index'),
+    url(r'^login/$',        'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^signup/$',       'news.views.signup', name='signup'),
+    url(r'^signup/submit$',       'news.views.signup_submit', name='signup_submit'),
+    # logout
+    # newest
+    # submit thread
+    # read thread
+    # edit profile
 )
