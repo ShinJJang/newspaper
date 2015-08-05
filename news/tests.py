27cf20d90297193bf5c25d06ec55edc499c8d500
Shin JJang <makao.rule@gmail.com>
2015-08-05 18:35:51 +0900

    Test title parser module

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from news.parser import parse_title
 
-# Create your tests here.
+
+class ParseTest(TestCase):
+
+    def test_parse_google_title(self):
+        google_title = parse_title('http://www.google.com')
+        self.assertEqual('Google', google_title)
