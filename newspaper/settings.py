02256e6f15ac48657a646d7cfc18388eff0d3d46
Shin JJang <makao.rule@gmail.com>
2015-08-05 14:16:53 +0900

    Add empty index page

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
-)
+) + ('news',)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- newspaper/urls.py
+++ newspaper/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'news.views.index', name='index'),
 )
