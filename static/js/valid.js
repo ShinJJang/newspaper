208fd7a7df6b47f65c752050c9e62e96979d1419
Shin JJang <makao.rule@gmail.com>
2015-08-07 13:43:34 +0900

    Block html input, when submit thread, signup

--- static/js/valid.js
+++ static/js/valid.js
@@ -1,3 +1,29 @@
 function valid_test (data) {
     return /<[a-z][\s\S]*>/i.test(data);
 }
+
+$("#signup_form").submit(function () {
+    var form = $(this).serializeArray();
+    var data = {};
+    form.forEach(function (item) {
+        data[item.name] = item.value;
+    });
+    data = JSON.stringify(data);
+    if (valid_test(data)) {
+        alert("정상적인 값을 입력해주세요");
+        return false;
+    }
+});
+
+$("#thread_form").submit(function () {
+    var form = $(this).serializeArray();
+    var data = {};
+    form.forEach(function (item) {
+        data[item.name] = item.value;
+    });
+    data = JSON.stringify(data);
+    if (valid_test(data)) {
+        alert("정상적인 값을 입력해주세요");
+        return false;
+    }
+});
