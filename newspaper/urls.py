9833f27ecdb8061367dd40b506a7003d0d6962ca
luca.shin <luca.shin@idincu.com>
2017-01-11 15:19:19 +0900

    Compatibility with Python 3.5

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from news import views as news_views
 from tastypie.api import Api
 from news.api import CommentResource, UserResource, ThreadResource
 
@@ -8,22 +10,22 @@ v1_api.register(CommentResource())
 v1_api.register(UserResource())
 v1_api.register(ThreadResource())
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'newspaper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/',                 include(admin.site.urls)),
-    url(r'^$',                      'news.views.index', name='index'),
-    url(r'^login/$',                'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^signup/$',               'news.views.signup', name='signup'),
-    url(r'^signup/submit/$',        'news.views.signup_submit', name='signup_submit'),
-    url(r'^logout/$',               'news.views.user_logout', name='user_logout'),
-    url(r'^thread/new/$',           'news.views.new_thread', name='new_thread'),
-    url(r'^thread/submit/$',        'news.views.submit_thread', name='submit_thread'),
-    url(r'^thread/(?P<thread_id>\d+)/$', 'news.views.read_thread', name='read_thread'),
-    url(r'^thread/(?P<thread_id>\d+)/vote/$', 'news.views.vote', name='vote'),
+    url(r'^$',                      news_views.index, name='index'),
+    url(r'^login/$',                auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^signup/$',               news_views.signup, name='signup'),
+    url(r'^signup/submit/$',        news_views.signup_submit, name='signup_submit'),
+    url(r'^logout/$',               news_views.user_logout, name='user_logout'),
+    url(r'^thread/new/$',           news_views.new_thread, name='new_thread'),
+    url(r'^thread/submit/$',        news_views.submit_thread, name='submit_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/$', news_views.read_thread, name='read_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/vote/$', news_views.vote, name='vote'),
     url(r'^api/', include(v1_api.urls)),
     # edit profile
     # about
-)
+]
